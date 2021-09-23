9a25c83fa9fb9de85f6af048818d031da294770f
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-09-23 11:41:30 -0700

    added call logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,54 @@
-import "./App.css";
-import Banner from "../src/assets/chat.jpg";
+import Banner from "../src/assets/chatty.jpg";
 import { AppBar, Typography } from "@material-ui/core";
 import VideoPlayer from "./components/VideoPlayer";
 import Notification from "./components/Notification";
 import Options from "./components/Options";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <>
       <div>
-        <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
-          <AppBar position="static" color="inherit">
+        <div
+          className={classes.wrapper}
+          style={{
+            backgroundImage: `url(${Banner})`,
+            width: "100%",
+            height: "99vh",
+          }}
+        >
+          <AppBar className={classes.appBar} position="static" color="inherit">
             <Typography variant="h4" align="center">
-              Video Chatty
+              Chatty App
             </Typography>
           </AppBar>
           <VideoPlayer />
