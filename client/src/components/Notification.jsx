d9c98c52c9b0139f8c15d7a356cd79490e20b032
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-09-23 13:13:34 -0700

    video functionality

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,5 +1,23 @@
+import React, { useContext } from "react";
+import { Button } from "@material-ui/core";
+
+import { SocketContext } from "../SocketContext";
+
 const Notification = () => {
-  return <div>Notifications</div>;
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
+
+  return (
+    <>
+      {call.isReceivingCall && !callAccepted && (
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <h1>{call.name} is calling:</h1>
+          <Button variant="contained" color="primary" onClick={answerCall}>
+            Answer
+          </Button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Notification;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ContextProvider } from "./SocketContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMo>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMo>,
   document.getElementById("root")
 );
